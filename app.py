9137e583d3d6d23fbbaf58d0947fea63db0b1598
teooooooooo <94641679+teooooooooo@users.noreply.github.com>
2023-11-25 15:19:06 +0000

    added HTML and CSS

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_worlf():
-  return "Hello, teu!"
+def helo_tello():
+  return render_template('home.html')
   
 
 if __name__ == "__main__":
